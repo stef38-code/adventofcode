833ef1386b7d39219248b3d68ac10cb61df8fffe
stephane betton <stphane.betton@ag2rlamondiale.fr>
2021-11-22 14:08:15 +0100

    first

--- src/main/java/org/advent/of/code/AdventOfCode.java
+++ src/main/java/org/advent/of/code/AdventOfCode.java
@@ -1,5 +1,9 @@
 package org.advent.of.code;
 
-public class AdventOfCode {
-    String url_input ="https://adventofcode.com/{0}/day/{1}/input";
+public class AdventOfCode extends MyHttp{
+    String url_entree ="https://adventofcode.com/{0}/day/{1}/input";
+    String url_reponse ="https://adventofcode.com/{0}/day/{1}/answer";
+    String cookieSession ="";
+
+
 }
